b56062452dea6ab22e4a41855d751e8b823d409f
Sam Granieri <sam@samgranieri.com>
2015-02-05 16:29:36 -0600

    Remove all the puts statements
    Less code is better code

--- lib/clearance_http_auth/middleware.rb
+++ lib/clearance_http_auth/middleware.rb
@@ -16,22 +16,12 @@ module Clearance
       # if the incoming request is targeting the API.
       #
       def call(env)
-        start_time = Time.now
-        #puts "=== clearance http auth:"
-        #puts "    env['HTTP_ACCEPT'] = #{env['HTTP_ACCEPT'].inspect}"
-        #puts "    env['CONTENT_TYPE'] = #{env['CONTENT_TYPE'].inspect}"
-        #puts "    env['HTTP_AUTHORIZATION'] = #{env['HTTP_AUTHORIZATION']}"
-        #puts "    targeting_api?(env) = #{targeting_api?(env)}"
         if targeting_api?(env) and (env['HTTP_AUTHORIZATION'] or Configuration.bypass_auth_without_credentials == false)
           if env['HTTP_ACCEPT'].nil?
             env['HTTP_ACCEPT'] = '*/*'
-            #puts "    env['HTTP_ACCEPT'] = #{env['HTTP_ACCEPT'].inspect} (after targeted api nil fix)"
           end
           @app = Rack::Auth::Basic.new(@app) do |username, password|
-            result = env[:clearance].sign_in ::User.authenticate(username, password)
-            #puts "    sign_in = #{result}"
-            #puts "    elapsed = #{"%0.5f" % (Time.now - start_time)} sec"
-            result
+            env[:clearance].sign_in ::User.authenticate(username, password)
           end
         end
         @app.call(env)
@@ -45,11 +35,9 @@ module Clearance
           return true if format && Configuration.api_formats.include?(format)
         end
         if Configuration.http_accept_matching
-          #puts "=== env['HTTP_ACCEPT'] = #{env['HTTP_ACCEPT'].inspect}"
           return true if Configuration.api_formats.any?{|format| env['HTTP_ACCEPT'] =~ /application\/#{format}/i}
         end
         if Configuration.content_type_matching
-          #puts "=== env['CONTENT_TYPE'] = #{env['CONTENT_TYPE'].inspect}"
           return true if Configuration.api_formats.any?{|format| env['CONTENT_TYPE'] =~ /application\/#{format}/i}
         end
         return false
